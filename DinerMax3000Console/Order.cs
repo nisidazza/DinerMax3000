32d3c4fe8c8b12cbc11bf67b3730e0525ed2f2ba
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-06-25 15:06:23 +1200

    add a Total property that calculate the items' total amount

--- DinerMax3000Console/Order.cs
+++ DinerMax3000Console/Order.cs
@@ -9,5 +9,18 @@ namespace DinerMax3000Console
     public class Order
     {
         public List<MenuItem> items = new List<MenuItem>();
+
+        public double Total
+        {
+            // loop through every menu item in the list and calculate the total
+            get
+            {
+                double calculatedTotal = 0;
+                foreach (MenuItem item in items) {
+                    calculatedTotal += item.Price;
+                }
+                return calculatedTotal;
+            }
+        }
     }
 }
