60f4a10fc989877f66898a1845563537006885ee
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-06-25 14:07:00 +1200

    initial commit

---
+++ DinerMax3000Console/Menu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinerMax3000Console
+{
+    class Menu
+    {
+        public string Name;
+
+    }
+}
