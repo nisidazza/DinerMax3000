7cefc21eb3cd68ce7907532fc6ea0b2f40b115f7
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-06-26 17:53:01 +1200

    add state objects that count the views of Menu page and the total views in master page

--- DinerMax3000Web/DinerMax3000MasterPage.master.cs
+++ DinerMax3000Web/DinerMax3000MasterPage.master.cs
@@ -9,6 +9,11 @@ public partial class DinerMax3000MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        // Application state object
+        if (Application["TotalPageCount"] == null)
+        {
+            Application["TotalPageCount"] = 0;
+        }
+        Application["TotalPageCount"] = (int)Application["TotalPageCount"] + 1;
     }
 }
--- DinerMax3000Web/Menus.aspx.cs
+++ DinerMax3000Web/Menus.aspx.cs
@@ -7,8 +7,13 @@ using System.Web.UI.WebControls;
 
 public partial class Menus : System.Web.UI.Page
 {
+    //Session state object
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if (Session["MenusPageCount"] == null)
+        {
+            Session["MenusPageCount"] = 0;
+        }
+        Session["MenusPageCount"] = (int)Session["MenusPageCount"] + 1;
     }
 }
