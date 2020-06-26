f72d6e0f01110e124fa440cd366a8dc87b5c646f
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-06-26 15:18:08 +1200

    implement GetAllMenus() method

--- DinerMax3000BusinessLayer/Menu.cs
+++ DinerMax3000BusinessLayer/Menu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DinerMax3000.Business.dsDinerMax3000TableAdapters;
 
 namespace DinerMax3000.Business
 {
@@ -11,6 +12,35 @@ namespace DinerMax3000.Business
         public Menu() {
             items = new List<MenuItem>();
 
+        }
+
+        public static List<Menu> GetAllMenus()
+        {
+            //create instances
+            MenuTableAdapter taMenu = new MenuTableAdapter();
+            Menu_ItemTableAdapter taMenuItem = new Menu_ItemTableAdapter();
+
+            
+            var dtMenu = taMenu.GetData();
+            // list returned by the method
+            List<Menu> allMenus = new List<Menu>();
+
+            // iterate through all the menu rows
+            foreach(dsDinerMax3000.MenuRow menuRow in dtMenu.Rows)
+            {
+                Menu currentMenu = new Menu();
+                //set the value from row
+                currentMenu.Name = menuRow.Name;
+                allMenus.Add(currentMenu);
+
+                var dtMenuItems = taMenuItem.GetMenuItemsByMenuId(menuRow.Id);
+                foreach (dsDinerMax3000.Menu_ItemRow menuItemRow in dtMenuItems.Rows)
+                {
+                    currentMenu.AddMenuItems(menuItemRow.Name, menuItemRow.Description, menuItemRow.Price);
+                }
+            }
+
+            return allMenus;
         }
 
         public void AddMenuItems(string Title, string Description, double Price)
