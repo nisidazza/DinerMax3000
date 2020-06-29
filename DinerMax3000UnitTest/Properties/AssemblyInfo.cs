54cddb5d4b9ffcc5422ca4423c12f9e6644f2b47
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-06-29 14:37:14 +1200

    add simple example of unit testing

---
+++ DinerMax3000UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DinerMax3000UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DinerMax3000UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("46a157f3-0b94-4860-a190-ad15411d5a91")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DinerMax3000UnitTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DinerMax3000UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var resultMenus = DinerMax3000.Business.Menu.GetAllMenus();
+            Assert.IsTrue(resultMenus.Count > 0);
+
+            int countBeforeSave = resultMenus[0].Items.Count;
+            resultMenus[0].SaveNewMenuItem("UT_Name", "UT_Description", 10.00);
+            resultMenus = DinerMax3000.Business.Menu.GetAllMenus();
+
+            int countAfterSave = resultMenus[0].Items.Count;
+
+            Assert.IsTrue(countBeforeSave < countAfterSave);
+        }
+    }
+}
